b6e7889364c8174151d0e201c17933d73c84e6b1
Jhonata Rosa <>
2021-04-08 18:00:25 +0000

    .

--- main.c
+++ main.c
@@ -96,13 +96,14 @@ int sort2 = rand() % 4;
 return roupa;
 }
 
+//CRIA UMA ROUPA FALSA, QUE NA VERDADE PREENCHE AS POSIÇÕES "VAZIAS" DA LISTA
 Roupa criaRoupaFalsa() {
   Roupa roupa;
   roupa.cod=0;
   return roupa;
 }
 
-//CRIA UMA LISTA DE ROUPAS E VENDA
+//CRIA UMA LISTA DE ROUPAS PARA VENDA
 void criaRoupasD() {
   for (int i=0; i < 20; i++) {
 roupasD[i] = criaRoupa();
@@ -151,18 +152,25 @@ void realocaRoupasR() {
 
 /////////////////CLIENTESSSSSS
 
-//CLIENTE COMPRA A ROUPA DA PRIMEIRA POSICAO
-void clienteCompra() {
+void *t_clientes (void *arg) {
+  sleep(2);
+  int sort = rand() %2;
+  //CLIENTE COMPRA UMA ROUPA DA PRIMEIRA POSICAO DA LISTA DE DISPONIVEIS
+  if (sort == 0) {
   pthread_mutex_lock(&mutexDisponiveis);
    printf("Cliente %d está comprando a roupa %d\n", contCliente, roupasD[0].cod);
    contCliente++;
     roupasD[0] = criaRoupaFalsa();
+    for (int i = 1; i < 50; i++) {
+       if (roupasD[i-1].cod == 0) {
+     roupasD[i-1] = roupasD[i];
+     roupasD[i].cod = 0;
+       }
+     }
   pthread_mutex_unlock(&mutexDisponiveis);
-   realocaRoupasD();
-}
-
-//CLIENTE DOA UMA ROUPA PARA A LISTA DE REPAROS
-void clienteDoa () {
+  }
+  //CLIENTE DOA UMA ROUPA PARA A LISTA DE REPAROS
+  else {
   pthread_mutex_lock(&mutexReparos);
    for (int i = 0; i < 20; i++) {
    if (roupasR[i].cod == 0 ){
@@ -173,26 +181,34 @@ void clienteDoa () {
    }
    }
   pthread_mutex_unlock(&mutexReparos); 
-}
-
-void *t_clientes (void *arg) {
-  int sort = rand() %1;
-  if (sort == 0) {
-  clienteCompra();
-  }
-  else {
-  clienteDoa();
   }
   pthread_exit(NULL);
 }
 
-//VOLUNTARIO MOVE UMA ROUPA DE POSICAO ALEATORIA PARA A LISTA DE DISPONIVEIS
-void voluntarioMove () {
-  pthread_mutex_lock(&mutexDisponiveis);
-  for (int i =0; i < 50;i++) {
+void *y_voluntarios (void *arg) {
+  sleep(2);
+  int sort = rand() % 3;
+  switch (sort) {
+    
+    case 0:
+    //VOLUNTARIO DOA UMA ROUPA PARA A LISTA DE DISPONIVEIS
+    pthread_mutex_lock(&mutexDisponiveis);
+  for (int i = 0; i < 50; i++) {
+   if (roupasD[i].cod == 0){
+  printf("Voluntario %d está doando uma roupa %d\n", contVolun, contador);
+  contVolun++;
+  roupasD[i] = criaRoupa();
+  break;
+   }
+  }
+  pthread_mutex_unlock(&mutexDisponiveis);
+    break;
+
+    case 1:
+    // VOLUNTARIO MOVE UMA ROUPA DA LISTA DE REPAROS PARA AS DISPONIVEIS
+    pthread_mutex_lock(&mutexDisponiveis);
+    for (int i =0; i < 50;i++) {
     if (roupasD[i].cod == 0) {
-      pthread_mutex_lock(&mutexReparos);
-      //ARRUMAR AQUI EM BAIXO
       int roupasExistente;
       for (int j=0; j < 20; j++) {
       if (roupasR[j].cod != 0) {
@@ -205,59 +221,34 @@ void voluntarioMove () {
       printf("Voluntario %d está movendo a roupa %d\n", contVolun, roupasR[temp].cod);
       contVolun++;
       roupasD[i] = roupasR[temp];
-      pthread_mutex_unlock(&mutexDisponiveis);
       roupasR[temp] = criaRoupaFalsa();
       realocaRoupasR();
-      pthread_mutex_unlock(&mutexReparos);
+      pthread_mutex_unlock(&mutexDisponiveis);
     }
   }
-}
+    break;
 
-//VOLUNTARIO DOA UMA ROUPA PARA A LISTA DE DISPONIVEIS
-void voluntarioDoa () {
-  pthread_mutex_lock(&mutexDisponiveis);
-  for (int i = 0; i < 50; i++) {
-   if (roupasD[i].cod == 0){
-  printf("Voluntario %d está doando uma roupa %d\n", contVolun, contador);
-  contVolun++;
-  roupasD[i] = criaRoupa();
-  break;
-   }
-  }
-  pthread_mutex_unlock(&mutexDisponiveis);
-}
-
-//VOLUNTARIO REMOVE A ROUPA MAIS ANTIGA, DAS DISPONIVEIS
-void voluntarioRemove () {
-  pthread_mutex_lock(&mutexDisponiveis);
+    case 2:
+    //VOLUNTARIO REMOVE A ROUPA MAIS ANTIGA DA LISTA DE DISPONIVEIS
+     pthread_mutex_lock(&mutexDisponiveis);
   printf("Voluntario %d está removendo a roupa %d\n", contVolun, roupasD[0].cod);
   contVolun++;
   roupasD[0].cod = 0;
    realocaRoupasD();
    pthread_mutex_unlock(&mutexDisponiveis);
-}
-
-void *y_voluntarios (void *arg) {
-  
-  int sort = rand() % 2;
-  switch (sort) {
-    case 0:
-    voluntarioDoa();
-    break;
-
-    case 1:
-    voluntarioMove();
-    break;
-
-    case 2:
-    voluntarioRemove();
     break;
   }
   pthread_exit(NULL); 
 }
 
+/**OBSERVAÇÕES
+O programa sempre aparece com problema de "segmentation fault", que não conseguimos identificar o que seja.
+E aparentemente, não sabemos se por relação ao problema anterior, mas a função rand() parece não funcionar, já pesquisamos possíveis erros, mas não encontramos nada sobre esse problema em C.
+Usando o valgrind linux, ele nos relata um possível erro também no trecho de código que corresponde ao que o Voluntario move uma peça da lista de reparos para a lista de disponiveis, mais precisamente o Case 1 do *y_voluntarios.
+
+**/
 int main(void) {
-  printf("VAII\n");
+  printf("Agora vai\n");
   criaRoupasD();
   criaRoupasR();
   
@@ -266,6 +257,8 @@ int main(void) {
 	pthread_t y[VOLUNTARIOS];
 	pthread_mutex_init(&mutexDisponiveis, NULL);
 	pthread_mutex_init(&mutexReparos, NULL);
+  pthread_mutex_init(&mutexContador, NULL);
+
 
   for (int i = 0; i < CLIENTES + VOLUNTARIOS; i++){
 	//printf("Thread %d sendo criada\n",i);
@@ -279,6 +272,12 @@ int main(void) {
 		pthread_join(t[i],NULL);
 		pthread_join(y[i],NULL); 
 	}
+
+  printf("Threads terminaram\n");
+
+	pthread_mutex_destroy(&mutexDisponiveis);
+	pthread_mutex_destroy(&mutexReparos);
+	pthread_mutex_destroy(&mutexContador);
 
   return 0;
 }
